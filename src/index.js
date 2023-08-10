22e615a401cde582d2764ddb458a0451bac540e9
zhangenming <282126346@qq.com>
2023-08-10 15:22:20 +0800

    use lazy function replace switch

--- src/index.js
+++ src/index.js
@@ -279,33 +279,33 @@ class Dayjs {
     })
 
     const matches = {
-      YY: String(this.$y).slice(-2),
-      YYYY: this.$y,
-      M: $M + 1,
-      MM: Utils.s($M + 1, 2, '0'),
-      MMM: getShort(locale.monthsShort, $M, months, 3),
-      MMMM: getShort(months, $M),
-      D: this.$D,
-      DD: Utils.s(this.$D, 2, '0'),
-      d: String(this.$W),
-      dd: getShort(locale.weekdaysMin, this.$W, weekdays, 2),
-      ddd: getShort(locale.weekdaysShort, this.$W, weekdays, 3),
-      dddd: weekdays[this.$W],
-      H: String($H),
-      HH: Utils.s($H, 2, '0'),
-      h: get$H(1),
-      hh: get$H(2),
-      a: meridiemFunc($H, $m, true),
-      A: meridiemFunc($H, $m, false),
-      m: String($m),
-      mm: Utils.s($m, 2, '0'),
-      s: String(this.$s),
-      ss: Utils.s(this.$s, 2, '0'),
-      SSS: Utils.s(this.$ms, 3, '0'),
-      Z: zoneStr // 'ZZ' logic below
+      YY: () => String(this.$y).slice(-2),
+      YYYY: () => Utils.s(this.$y, 4, '0'),
+      M: () => $M + 1,
+      MM: () => Utils.s($M + 1, 2, '0'),
+      MMM: () => getShort(locale.monthsShort, $M, months, 3),
+      MMMM: () => getShort(months, $M),
+      D: () => this.$D,
+      DD: () => Utils.s(this.$D, 2, '0'),
+      d: () => String(this.$W),
+      dd: () => getShort(locale.weekdaysMin, this.$W, weekdays, 2),
+      ddd: () => getShort(locale.weekdaysShort, this.$W, weekdays, 3),
+      dddd: () => weekdays[this.$W],
+      H: () => String($H),
+      HH: () => Utils.s($H, 2, '0'),
+      h: () => get$H(1),
+      hh: () => get$H(2),
+      a: () => meridiemFunc($H, $m, true),
+      A: () => meridiemFunc($H, $m, false),
+      m: () => String($m),
+      mm: () => Utils.s($m, 2, '0'),
+      s: () => String(this.$s),
+      ss: () => Utils.s(this.$s, 2, '0'),
+      SSS: () => Utils.s(this.$ms, 3, '0'),
+      Z: () => zoneStr // 'ZZ' logic below
     }
 
-    return str.replace(C.REGEX_FORMAT, (match, $1) => $1 || matches[match] || zoneStr.replace(':', '')) // 'ZZ'
+    return str.replace(C.REGEX_FORMAT, (match, $1) => $1 || matches[match]?.() || zoneStr.replace(':', '')) // 'ZZ'
   }
 
   utcOffset() {
